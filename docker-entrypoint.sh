9e2c57003adb9c11f0a2b6cbb9ec4a9fb880f5ec
Tobias Haider <thai@kollabor.at>
2021-06-01 22:37:08 +0200

    fixing typo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -31,7 +31,7 @@ if [ ! -e $CONTAINER_1_35 ]; then
 
     echo "IMPORTING SEMORG PAGES..."
     php maintenance/importDump.php < extensions/SemanticOrganization/import/semorg_pages.xml
-    php maintenance/importDump.php < additonal-pages.xml
+    php maintenance/importDump.php < additional-pages.xml
 
     echo "CLEANUP..."
     php maintenance/rebuildrecentchanges.php
